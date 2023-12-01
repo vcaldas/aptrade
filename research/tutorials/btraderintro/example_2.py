fac21071cf61591577a6804557fe2c78d06a0d39
Victor Caldas <caldas.victor@gmail.com>
2023-12-01 22:42:43 +0100

    add examples

--- research/tutorials/btraderintro/example_2.py
+++ research/tutorials/btraderintro/example_2.py
@@ -1,35 +1,40 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
-import sys
-import os
+
+import datetime  # For datetime objects
+import os.path  # To manage paths
+import sys  # To find out the script name (in argv[0])
+
+
+# Import the backtrader platform
 import backtrader as bt
-import datetime
-from strategy import TestStrategy, TestStrategybase
+
+
+# Create a Stratey
+
 
 if __name__ == "__main__":
     # Create a cerebro entity
     cerebro = bt.Cerebro()
 
     # Add a strategy
-    strats = cerebro.optstrategy(
-        TestStrategybase, maperiod=range(10, 31), printlog=True
-    )
+    strats = cerebro.optstrategy(TestStrategy, maperiod=range(10, 31))
 
-    # Add a strategy
-    # cerebro.addstrategy(TestStrategy)
-
-    print("Starting Portfolio Value: %.2f" % cerebro.broker.getvalue())
-
+    # Datas are in a subfolder of the samples. Need to find where the script is
+    # because it could have been called from anywhere
     modpath = os.path.dirname(os.path.abspath(sys.argv[0]))
     datapath = os.path.join(modpath, "./orcl-1995-2014.txt")
 
+    # Create a Data Feed
     data = bt.feeds.YahooFinanceCSVData(
         dataname=datapath,
         # Do not pass values before this date
         fromdate=datetime.datetime(2000, 1, 1),
+        # Do not pass values before this date
+        todate=datetime.datetime(2000, 12, 31),
         # Do not pass values after this date
-        todate=datetime.datetime(2001, 1, 2),
         reverse=False,
     )
+
     # Add the Data Feed to Cerebro
     cerebro.adddata(data)
 
