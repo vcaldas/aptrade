369b7ece440fdfebeda6a8227f813e8a65fb2cab
Victor Caldas <victor_caldas@cargill.com>
2024-03-12 09:35:58 +0100

    Move to Django

--- webapp/boot.sh
+++ webapp/boot.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # this script is used to boot a Docker container
 while true; do
-    flask db upgrade
+    python manage.py migrate
     if [[ "$?" == "0" ]]; then
         break
     fi
     echo Deploy command failed, retrying in 5 secs...
     sleep 5
 done
-exec gunicorn -b :8000 --access-logfile - --error-logfile - server:app --reload
+python manage.py runserver --host 0.0.0.0:8000
