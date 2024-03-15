f17bc3cbbcb11691db61f80487817c8c4e457bb2
Victor Caldas <caldas.victor@gmail.com>
2024-03-15 23:46:54 +0100

    Wip - populating feeds

--- webapp/rssfeed/urls.py
+++ webapp/rssfeed/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("update", views.update, name="update"),
+    path("<int:entry_id>/", views.detail, name="detail"),
+    # ex: /rssfeed/5/results/
+    path("<int:entry_id>/results/", views.results, name="results"),
 ]
--- webapp/rssfeed/views.py
+++ webapp/rssfeed/views.py
@@ -3,6 +3,35 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+from django.http import Http404
+from django.template import loader
+
+from .models import RSSSource
+
+# context = {"latest_question_list": latest_question_list}
+#     return render(request, "polls/index.html", context)
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the feeds index.")
+    feeds_list = RSSSource.objects.order_by("-pub_date")[:5]
+    context = {
+        "feeds_list": feeds_list,
+    }
+    return render(request, "feeds/index.html", context)
+
+def detail(request, entry_id):
+    return HttpResponse("You're looking at entry %s." % entry_id)
+
+
+def results(request, entry_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % entry_id)
+
+
+def update(request):
+    response = "Updating feeds."
+    feeds_list = RSSSource.objects.order_by("-pub_date")
+    for feed in feeds_list:
+        print(feed)
+    print("response test")
+    return HttpResponse(response)
+    
