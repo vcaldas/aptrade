664c19f535c703731104e7beee82266b3965dd21
Victor Caldas <caldas.victor@gmail.com>
2026-07-10 23:58:13 +0200

    style fixes

--- src/aptrade/filters/datafiller.py
+++ src/aptrade/filters/datafiller.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# -*- coding: utf-8; py-indent-offset:4 -*-
 ###############################################################################
 #
 # Copyright (C) 2015-2023 Daniel Rodriguez
@@ -18,7 +17,6 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ###############################################################################
-from __future__ import absolute_import, division, print_function, unicode_literals
 
 import collections
 from datetime import datetime, timedelta
@@ -58,7 +56,7 @@ class DataFiller(AbstractDataBase):
     )
 
     def start(self):
-        super(DataFiller, self).start()
+        super().start()
         self._fillbars = collections.deque()
         self._dbar = False
 
@@ -73,7 +71,7 @@ class DataFiller(AbstractDataBase):
         self.p.timeframe = self._timeframe = self.p.dataname._timeframe
         self.p.compression = self._compression = self.p.dataname._compression
 
-        super(DataFiller, self).preload()
+        super().preload()
 
     def _copyfromdata(self):
         # Data is allowed - Copy size which is "number of lines"
--- src/aptrade/filters/datafilter.py
+++ src/aptrade/filters/datafilter.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# -*- coding: utf-8; py-indent-offset:4 -*-
 ###############################################################################
 #
 # Copyright (C) 2015-2023 Daniel Rodriguez
@@ -18,7 +17,6 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ###############################################################################
-from __future__ import absolute_import, division, print_function, unicode_literals
 
 import aptrade as bt
 
@@ -52,7 +50,7 @@ class DataFilter(bt.AbstractDataBase):
         self.p.timeframe = self._timeframe = self.p.dataname._timeframe
         self.p.compression = self._compression = self.p.dataname._compression
 
-        super(DataFilter, self).preload()
+        super().preload()
 
     def _load(self):
         if not len(self.p.dataname):
