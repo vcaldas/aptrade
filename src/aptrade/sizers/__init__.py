7c8c8cca2c4f3f5a5d37eaac6fd592b2fe0c55c0
Victor Caldas <caldas.victor@gmail.com>
2026-07-13 10:43:59 +0200

    feat: trigger docs

--- src/aptrade/sizers/__init__.py
+++ src/aptrade/sizers/__init__.py
@@ -23,9 +23,6 @@ from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from typing import Any, ClassVar
 
-# from aptrade.metabase import MetaParams
-
-
 @dataclass(slots=True, frozen=True)
 class EmptyParams:
     """Default parameter set."""
@@ -91,80 +88,6 @@ class PositionSizer(ABC):
             Integer size to execute.
         """
         raise NotImplementedError
-
-
-# class AbstractSizer(ABC):
-#     """
-#     The Abstract Factory interface declares a set of methods that return
-#     different abstract products. These products are called a family and are
-#     related by a high-level theme or concept. Products of one family are usually
-#     able to collaborate among themselves. A family of products may have several
-#     variants, but the products of one variant are incompatible with products of
-#     another.
-#     """
-
-#     strategy = None
-#     broker = None
-
-#     def getsizing(self, data, isbuy: bool):
-#         comminfo = self.broker.getcommissioninfo(data)
-#         return self._getsizing(comminfo, self.broker.getcash(), data, isbuy)
-
-#     def __init__(self, *args, **kwargs):
-#         """Initialize params for sizers that declare a `params` tuple.
-
-#         Supports positional and keyword parameters to remain compatible
-#         with the previous `MetaParams`-based behavior used by sizers.
-#         """
-#         params_def = getattr(self.__class__, "params", ()) or ()
-#         # Start with defaults
-#         params_obj = SimpleNamespace()
-#         param_names = [name for name, _ in params_def]
-#         for name, default in params_def:
-#             setattr(params_obj, name, default)
-
-#         # Apply positional args in order
-#         for name, val in zip(param_names, args, strict=False):
-#             setattr(params_obj, name, val)
-
-#         # Apply keyword overrides
-#         for name in param_names:
-#             if name in kwargs:
-#                 setattr(params_obj, name, kwargs.pop(name))
-
-#         # expose as both `params` and `p` for compatibility
-#         self.params = params_obj
-#         self.p = params_obj
-
-#     @abstractmethod
-#     def _getsizing(self, comminfo, cash, data, isbuy: bool) -> int:
-#         """This method has to be overriden by subclasses of Sizer to provide
-#         the sizing functionality
-
-#         Params:
-#           - ``comminfo``: The CommissionInfo instance that contains
-#             information about the commission for the data and allows
-#             calculation of position value, operation cost, commision for the
-#             operation
-
-#           - ``cash``: current available cash in the *broker*
-
-#           - ``data``: target of the operation
-
-#           - ``isbuy``: will be ``True`` for *buy* operations and ``False``
-#             for *sell* operations
-
-#         The method has to return the actual size (an int) to be executed. If
-#         ``0`` is returned nothing will be executed.
-
-#         The absolute value of the returned value will be used
-
-#         """
-#         raise NotImplementedError
-
-#     def set(self, strategy, broker):
-#         self.strategy = strategy
-#         self.broker = broker
 
 
 from .fixedsize import *  # noqa: F403, F401, E402
