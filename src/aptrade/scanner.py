288f35d5f83460ac09ae4f65e75b95417e51a66c
Victor Caldas <caldas.victor@gmail.com>
2026-07-23 11:45:00 +0200

    feat: addrss 3.14 build error from non pickable objects

--- src/aptrade/scanner.py
+++ src/aptrade/scanner.py
@@ -18,7 +18,6 @@ from aptrade.core.alerts import AlertEvent, get_alert_publisher
 from aptrade.core.config import settings
 from aptrade.feeds.massive_live import setup_massive_live_feeds
 from aptrade.plot import Plot
-from aptrade.private.strategies.ORB import OpeningRangeBreakout
 from aptrade.sizers import SimpleSizer
 from aptrade.strategies.BuyAndHold import BuyAndHold
 
@@ -107,8 +106,7 @@ def _build_scanner_runtime(
     # logger.info("[scanner-checkpoint] build_runtime:before_ibbroker_init")
 
     logger.info("[scanner-checkpoint] build_runtime:after_setbroker")
-    # cerebro.addstrategy(BuyAndHold)
-    cerebro.addstrategy(OpeningRangeBreakout)
+    cerebro.addstrategy(BuyAndHold)
     logger.info("[scanner-checkpoint] build_runtime:after_addstrategy")
     cerebro.addsizer(SimpleSizer, percents=10)
     logger.info("[scanner-checkpoint] build_runtime:after_addsizer")
